db6da5078da1d3cbc32699008c620dc7b9b39ec3
nolanG <nolanrgiles@gmail.com>
2019-12-15 17:32:00 -0500

    Added functionality for a similar TV api lookup as well as updated the loaded booleans

--- worthwatching/src/components/ShowDetails.js
+++ worthwatching/src/components/ShowDetails.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 
 //api calls
-import { SearchTvById } from '../services/api-helper'
+import { SearchTvById, SearchSimilarTvById } from '../services/api-helper'
 
 class ShowDetails extends Component {
   constructor(props) {
@@ -9,10 +9,12 @@ class ShowDetails extends Component {
     
     this.state = {
       idResults: [],
+      similarResults: [],
       baseImgPath: "https://image.tmdb.org/t/p/w",
       imgSize: "300",
       id: this.props.match.params.showId,
-      hasLoaded: false,
+      hasDetailsLoaded: false,
+      hasSimilarLoaded: false,
       isMiniseries: false,
       creator: "",
       network: "",
@@ -21,6 +23,15 @@ class ShowDetails extends Component {
   }
   async componentDidMount() {
     await this.getDetails();
+    await this.getSimilar();
+  }
+
+  getSimilar = async () => {
+    const similarResults = await SearchSimilarTvById(this.state.id);
+    this.setState({
+      similarResults,
+      hasSimilarLoaded: true,
+    })
   }
 
   getDetails = async () => {
@@ -47,7 +58,7 @@ class ShowDetails extends Component {
     }
     this.setState({
       idResults,
-      hasLoaded: true
+      hasDetailsLoaded: true
     })
   }
 
@@ -55,40 +66,42 @@ class ShowDetails extends Component {
     // console.log(this.state.idResults)
     return (
       <div className="details">
-        <div className="details-img">
-          <img src={`${this.state.baseImgPath}${this.state.imgSize}${this.state.idResults.poster_path}`} alt="tvPoster" />
-        </div>
-        <div className="details-text">
-          {this.state.hasLoaded &&
-          <div>
-            <div className="details-title">
-              {this.state.idResults.name}
-            </div>
-            <div className="details-overview">
-              {this.state.idResults.overview}
-            </div>
-            
-            <div className="details-info">
-              <h3>Show Info:</h3>
-              <div>
-                {this.state.isMiniseries &&
-                this.state.idResults.type}
-              </div>
-              {this.state.creator &&
-                <div>Created by: {this.state.creator}</div>
-              }
-              {this.state.network &&
-                <div>Where to watch: {this.state.network}</div>
-              }
-              <div>Run time: {this.state.idResults.episode_run_time[0]} minutes</div>
-              <div>Episodes: {this.state.idResults.number_of_episodes}</div>
-              <div>Seasons: {this.state.idResults.number_of_seasons}</div>
-              <div>Originally aired on: {this.state.idResults.first_air_date}</div>
-              {this.state.ended && <div>No longer on the air</div>}
-              {this.state.idResults.next_episode_to_air && <div>Next Episode: {this.state.idResults.next_episode_to_air.name} @ {this.state.idResults.next_episode_to_air.air_date}</div>}
-            </div>
+        <div className="details-img-and-text-wrapper">
+          <div className="details-img">
+            <img src={`${this.state.baseImgPath}${this.state.imgSize}${this.state.idResults.poster_path}`} alt="tvPoster" />
           </div>
-          }
+          <div className="details-text">
+            {this.state.hasDetailsLoaded &&
+            <div>
+              <div className="details-title">
+                {this.state.idResults.name}
+              </div>
+              <div className="details-overview">
+                {this.state.idResults.overview}
+              </div>
+              
+              <div className="details-info">
+                <h3>Show Info:</h3>
+                <div>
+                  {this.state.isMiniseries &&
+                  this.state.idResults.type}
+                </div>
+                {this.state.creator &&
+                  <div>Created by: {this.state.creator}</div>
+                }
+                {this.state.network &&
+                  <div>Where to watch: {this.state.network}</div>
+                }
+                <div>Run time: {this.state.idResults.episode_run_time[0]} minutes</div>
+                <div>Episodes: {this.state.idResults.number_of_episodes}</div>
+                <div>Seasons: {this.state.idResults.number_of_seasons}</div>
+                <div>Originally aired on: {this.state.idResults.first_air_date}</div>
+                {this.state.ended && <div>No longer on the air</div>}
+                {this.state.idResults.next_episode_to_air && <div>Next Episode: {this.state.idResults.next_episode_to_air.name} @ {this.state.idResults.next_episode_to_air.air_date}</div>}
+              </div>
+            </div>
+            }
+          </div>
         </div>
       </div>
     )
