320a81c6ce01cdf87712e243a595f225b14d0df7
nolanG <nolanrgiles@gmail.com>
2019-12-15 12:06:06 -0500

    Show now links appropriately with showId variable param

--- worthwatching/src/components/Show.js
+++ worthwatching/src/components/Show.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
+//react-router
+import { Link } from 'react-router-dom'
+
 const Show = (props) => {
 //for img src add 200, 300, 400, 500..etc to change the size of the poster img.
   return (
-    <div className="show">
-        <div className="show-img">
-          <img src={`${props.imgPath}${props.imgSize}${props.show.poster_path}`} alt="tvPoster" />
-        </div>
-        <div className="show-title">
-          {props.show.name}
-        </div>
+    <div className="show-wrapper">
+      <div className="show">
+        <Link to={{pathname: `/details/${props.show.id}`}}>
+          <div className="show-img">
+            <img src={`${props.imgPath}${props.imgSize}${props.show.poster_path}`} alt="tvPoster" />
+          </div>
+        </Link>
+          <div className="show-title">
+            {props.show.name}
+          </div>
+      </div>
     </div>
   )
 }
