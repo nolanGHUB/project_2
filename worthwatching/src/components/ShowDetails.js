d19e31dc2d2488e641eb7a08959aa478b16846e5
nolanG <nolanrgiles@gmail.com>
2019-12-15 15:44:13 -0500

    more elements and re-positioning for flex display on ShowDetails component

--- worthwatching/src/components/ShowDetails.js
+++ worthwatching/src/components/ShowDetails.js
@@ -55,38 +55,41 @@ class ShowDetails extends Component {
     // console.log(this.state.idResults)
     return (
       <div className="details">
-        {this.state.hasLoaded &&
-        <div>
-          <div className="details-title">
-            {this.state.idResults.name}
-          </div>
-          <div className="details-overview">
-            {this.state.idResults.overview}
-          </div>
-          <div className="details-img">
-            <img src={`${this.state.baseImgPath}${this.state.imgSize}${this.state.idResults.poster_path}`} alt="tvPoster" />
-          </div>
-          <div className="details-info">
-            <h3>Show Info:</h3>
-            <div>
-              {this.state.isMiniseries &&
-              this.state.idResults.type}
-            </div>
-            {this.state.creator &&
-              <div>Created by: {this.state.creator}</div>
-            }
-            {this.state.network &&
-              <div>Where to watch: {this.state.network}</div>
-            }
-            <div>Run time: {this.state.idResults.episode_run_time[0]} minutes</div>
-            <div>Episodes: {this.state.idResults.number_of_episodes}</div>
-            <div>Seasons: {this.state.idResults.number_of_seasons}</div>
-            <div>Originally aired on: {this.state.idResults.first_air_date}</div>
-            {this.state.ended && <div>No longer on the air</div>}
-            {this.state.idResults.next_episode_to_air && <div>Next Episode: {this.state.idResults.next_episode_to_air.name} @ {this.state.idResults.next_episode_to_air.air_date}</div>}
-          </div>
+        <div className="details-img">
+          <img src={`${this.state.baseImgPath}${this.state.imgSize}${this.state.idResults.poster_path}`} alt="tvPoster" />
         </div>
-        }
+        <div className="detail-text">
+          {this.state.hasLoaded &&
+          <div>
+            <div className="details-title">
+              {this.state.idResults.name}
+            </div>
+            <div className="details-overview">
+              {this.state.idResults.overview}
+            </div>
+            
+            <div className="details-info">
+              <h3>Show Info:</h3>
+              <div>
+                {this.state.isMiniseries &&
+                this.state.idResults.type}
+              </div>
+              {this.state.creator &&
+                <div>Created by: {this.state.creator}</div>
+              }
+              {this.state.network &&
+                <div>Where to watch: {this.state.network}</div>
+              }
+              <div>Run time: {this.state.idResults.episode_run_time[0]} minutes</div>
+              <div>Episodes: {this.state.idResults.number_of_episodes}</div>
+              <div>Seasons: {this.state.idResults.number_of_seasons}</div>
+              <div>Originally aired on: {this.state.idResults.first_air_date}</div>
+              {this.state.ended && <div>No longer on the air</div>}
+              {this.state.idResults.next_episode_to_air && <div>Next Episode: {this.state.idResults.next_episode_to_air.name} @ {this.state.idResults.next_episode_to_air.air_date}</div>}
+            </div>
+          </div>
+          }
+        </div>
       </div>
     )
   }
