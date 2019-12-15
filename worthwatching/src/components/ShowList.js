7996f81a53264a3875a708f1cae02680e210f50f
nolanG <nolanrgiles@gmail.com>
2019-12-15 12:07:37 -0500

    Removed querySelector for removing empty divs, functionality now done in Home.js

--- worthwatching/src/components/ShowList.js
+++ worthwatching/src/components/ShowList.js
@@ -3,24 +3,23 @@ import React from 'react'
 //custom components
 import Show from './Show'
 
+
 const ShowList = (props) => {
   return (
-    <div className="show-list-wrapper">
-      <div className="show-list">
-        {props.shows.map((show, key) =>
-          <div className="show-wrapper" key={key}>
-            {show.poster_path && show.vote_count > 0 && show.overview &&
-            <Show 
-              imgPath={props.imgPath}
-              show={show}
-              imgSize={props.imgSize}
-            />
-            }
-          </div>
-        )}
+      <div className="show-list-wrapper">
+        <div className="show-list">
+          {props.shows.map((show, key) =>
+            <div key={key}>
+                <Show 
+                  imgPath={props.imgPath}
+                  show={show}
+                  imgSize={props.imgSize}
+                />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  )
+    )
 }
 
 export default ShowList
