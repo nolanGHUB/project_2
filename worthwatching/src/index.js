d79c1b846b29852efdd339e3511b99a38b8a2fd3
nolanG <nolanrgiles@gmail.com>
2019-12-16 11:14:56 -0500

    Added a ScrollToTop routing component into index.js to force scrolling to top of page when components are re-rendering via route to themselves

--- worthwatching/src/index.js
+++ worthwatching/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ScrollToTop from './ScrollToTop'
 import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -9,6 +10,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 ReactDOM.render(
   
   <Router>
+    <ScrollToTop />
     <App />
   </Router>, document.getElementById('root'));
 
