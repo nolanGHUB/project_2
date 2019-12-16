d6a23b69fd0042be242ac2379e3a76382cf9d726
nolanG <nolanrgiles@gmail.com>
2019-12-15 21:53:43 -0500

    Created boilerplate & basic functionality for displaying trending TV

---
+++ worthwatching/src/components/Trending.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+//custom components
+import ShowList from './ShowList'
+
+//api calls
+import { SearchTrendingTv } from '../services/api-helper'
+
+class Trending extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      trendingTv: [],
+      baseImgPath: "https://image.tmdb.org/t/p/w",
+      imgSize: "200",
+      hasLoaded: false,
+    }
+  }
+
+  componentDidMount = async () => {
+    await this.getTrending();
+  }
+
+  getTrending = async () => {
+    const trendingTv = await SearchTrendingTv()
+    this.setState({
+      trendingTv,
+      hasLoaded: true
+    })
+  }
+
+  render() {
+    console.log(this.state.trendingTv)
+    return (
+      <div className="trending">
+        {this.state.hasLoaded &&
+          <ShowList
+            shows={this.state.trendingTv}
+            imgPath={this.state.baseImgPath}
+            imgSize={this.state.imgSize}
+          />
+        }
+      </div>
+    )
+  }
+  
+}
+
+export default Trending
