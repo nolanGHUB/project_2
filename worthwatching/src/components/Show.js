89792babb84eb29dd9575bc3f62a5867637fc3c6
nolanG <nolanrgiles@gmail.com>
2019-12-18 14:40:16 -0500

    Removed the ability to remove shows with no image from show, and moved into Similar

--- worthwatching/src/components/Show.js
+++ worthwatching/src/components/Show.js
@@ -9,11 +9,11 @@ const Show = (props) => {
     <div className="show-wrapper">
       <div className="show">
         <Link to={{ pathname: `/details/${props.show.id}` }}>
-          {props.show.poster_path && (
-            <div className="show-img">
-              <img src={`${props.imgPath}${props.imgSize}${props.show.poster_path}`} alt="tvPoster" />
-            </div>
-          )}
+          {/* {props.show.poster_path && ( */}
+          <div className="show-img">
+            <img src={`${props.imgPath}${props.imgSize}${props.show.poster_path}`} alt="tvPoster" />
+          </div>
+          {/* )} */}
         </Link>
         <div className="show-title">
           {props.show.name}
