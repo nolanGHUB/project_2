35ad2d692655ccbcacc71dd31e653430d5623f17
nolanG <nolanrgiles@gmail.com>
2019-12-20 08:52:28 -0500

    component to handle hamburger functionality

---
+++ worthwatching/src/components/HamburgerMenu.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+
+//react-router
+import { NavLink } from 'react-router-dom';
+
+class HamburgerMenu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggleMenu() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div className="hamburger">
+        <i className="fa fa-bars ham-butt" onClick={() => this.toggleMenu()}></i>
+        <nav className={"ham-nav " + (this.state.isOpen ? 'open' : null)}>
+          <NavLink to="/" activeClassName="selected" onClick={this.closeMenu}>
+            Home
+            </NavLink>
+          <NavLink to="/trending" activeClassName="selected" onClick={this.closeMenu}>
+            Trending
+             </NavLink>
+          <NavLink to="/tonight" activeClassName="selected" onClick={this.closeMenu}>
+            Airing Today
+            </NavLink>
+          <NavLink to="/discovery" activeClassName="selected" onClick={this.closeMenu}>
+            Discovery
+            </NavLink>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default HamburgerMenu
